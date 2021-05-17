12d901387e86316f01ce4922710f98f274d1074d
arun kumar pokala <arunkumar444422@outlook.com>
2021-05-17 19:37:18 +0530

    MicroServices - Commit 2 7. Added Two MicroServices [Currency-Exchange and Currency-Conversion] 8. Added openFeign dependency [for web services clients integrations] to Invoke Currency-Exchange from Currency-Conversion

---
+++ microservices/currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,67 @@
+package com.example.microservices.currencyconversionservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+@RestController
+public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyExchangeProxy currencyExchangeProxy; //Proxy created for Currency-Exchange service using openFeign
+
+    //Using Rest Template to Communicate with Currency-Exchange Service
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity) {
+
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
+                "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                CurrencyConversion.class,
+                uriVariables);
+
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+        if (currencyConversion==null) {
+            throw new RuntimeException("Currency Exchange not found");
+        }
+        return new CurrencyConversion(
+                currencyConversion.getId(),
+                from ,
+                to,
+                currencyConversion.getConversionMultiple(),
+                quantity,
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment()  + " " + "rest template");
+    }
+
+    //Using openFeign to integrate clients (Currency-Exchange and Currency-Conversion)
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity) {
+
+        CurrencyConversion currencyConversion = currencyExchangeProxy.retrieveExchange(from, to);
+
+        return new CurrencyConversion(
+                currencyConversion.getId(),
+                from ,
+                to,
+                currencyConversion.getConversionMultiple(),
+                quantity,
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment() + " " + "feign");
+    }
+}
---
+++ microservices/currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,27 @@
+package com.example.microservices.currencyexchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private CurrencyExchangeRepository repository;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchange(@PathVariable String from, @PathVariable String to) {
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+        if(currencyExchange==null) {
+            throw new RuntimeException("Unable to Find data for " + from + " to "+ to);
+        }
+        currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
+        return currencyExchange;
+    }
+}
