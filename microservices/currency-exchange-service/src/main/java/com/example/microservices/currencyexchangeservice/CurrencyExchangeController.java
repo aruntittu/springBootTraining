16b7559aa9e9046c81f7ca79029cf872996a9f52
arun kumar pokala <arunkumar444422@outlook.com>
2021-05-20 11:43:33 +0530

    MicroServices - Commit 5 18. Installed Docker Locally and Learned concepts of Registry, Repository, Tag, Images and Containers 19. Understood Docker Architecture 20. Connected MicroServices [api-gateway, currency-exchange, currency-conversion] to Zipkin (Distributed Tracing) 21. Created Container Images for MicroServices[api-gateway, currency-exchange, currency-conversion, naming-server] 22. Created Docker Compose file for running multi-container docker applications [docker-compose.yaml] added Zipkin and RabbitMQ in composer.

--- microservices/currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ microservices/currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.example.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private Environment environment;
 
@@ -17,6 +21,9 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchange(@PathVariable String from, @PathVariable String to) {
+
+        logger.info("retrieveExchange called from {} to {}", from , to);
+
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
         if(currencyExchange==null) {
             throw new RuntimeException("Unable to Find data for " + from + " to "+ to);
